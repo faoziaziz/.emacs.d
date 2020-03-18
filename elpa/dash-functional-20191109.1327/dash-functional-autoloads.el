79077f1ceac768e514cb99443b4633487dce530a
Aziz Faozi <admin@labseni.com>
2020-03-18 15:20:27 +0700

    mare

---
+++ elpa/dash-functional-20191109.1327/dash-functional-autoloads.el
@@ -0,0 +1,16 @@
+;;; dash-functional-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("dash-functional.el") (24177 44336 72786
+;;;;;;  407000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-functional-autoloads.el ends here
